ebef29a894014506dcf00847f6a9252976d4f390
frub <benjamin.francheteau@cnieg.fr>
2023-12-13 15:29:02 +0100

    Correctif

--- src/file.ts
+++ src/file.ts
@@ -1,65 +1,71 @@
-import { cat } from "shelljs"
-import { z } from "zod"
+import { cat } from "shelljs";
+import { z } from "zod";
 
-export function jsonToSQL(json: any[], columns: Record<string, string>, upsertPrimaryKey: string) {
-    const values = json
-      .map((m) => {
-        const match = formatMatch(m, columns)
-        return `(${Object.values(match).join(',')})`
-      })
-      .join(',\n')
+export function jsonToSQL(
+  json: any[],
+  columns: Record<string, string>,
+  upsertPrimaryKey: string
+) {
+  const values = json
+    .map((m) => {
+      const match = formatMatch(m, columns);
+      return `(${Object.values(match).join(",")})`;
+    })
+    .join(",\n");
 
-    let suffixQuery =  '';
-    if(upsertPrimaryKey){
-      suffixQuery = `
+  let suffixQuery = "";
+  if (upsertPrimaryKey) {
+    suffixQuery = `
         ON CONFLICT (${upsertPrimaryKey}) DO UPDATE SET
-        ${Object.keys(columns).map((key)=>{
-          return `${key} = EXCLUDED.${key}`
+        ${Object.keys(columns).map((key) => {
+          return `${key} = EXCLUDED.${key}`;
         })} 
-      `
-    }
-    
-    return `
-    INSERT INTO matchs (${Object.keys(columns).join(',')})  
+      `;
+  }
+
+  return `
+    INSERT INTO matchs (${Object.keys(columns).join(",")})  
     VALUES ${values}
     ${suffixQuery}
-    ;`
+    ;`;
 }
 
 const leaguesSchema = z.record(
   z.string(),
-  z.array(z.object({
-    league: z.number(),
-    season: z.number(),
-  }))
-)
+  z.array(
+    z.object({
+      league: z.number(),
+      season: z.number(),
+    })
+  )
+);
 
 const configurationSchema = z.object({
   leagues: leaguesSchema,
-  columns: z.record(z.string(), z.string())
-})
+  columns: z.record(z.string(), z.string()),
+});
 
-export function getConfigurations(path: string){
-  return configurationSchema.parse(JSON.parse(cat(path)))
+export function getConfigurations(path: string) {
+  return configurationSchema.parse(JSON.parse(cat(path)));
 }
-
 
 export function formatMatch(match: any, columns: Record<string, string>) {
   return Object.keys(columns).reduce((acc, key) => {
-    const value= getValueByKey(match, columns[key]);
-    if(!value)
-      acc[key] = "NULL";
+    const value = getValueByKey(match, columns[key]);
+    if (!value && value !== 0) acc[key] = "NULL";
     else
-      acc[key] = `'${getValueByKey(match, columns[key]).toString().replace(/'/g, "''")}'`
-    return acc
-  }, {} as any)
-  
+      acc[key] = `'${getValueByKey(match, columns[key])
+        .toString()
+        .replace(/'/g, "''")}'`;
+    return acc;
+  }, {} as any);
 }
 
-function getValueByKey(obj: any, path: string){
-  const paths = path.split('.')
+function getValueByKey(obj: any, path: string) {
+  const paths = path.split(".");
   return paths.reduce((acc, key) => {
-    if(acc[key] === undefined) throw new Error(`key ${key} not found, path: ${path}`)
-    return acc[key]
-  }, obj)
+    if (acc[key] === undefined)
+      throw new Error(`key ${key} not found, path: ${path}`);
+    return acc[key];
+  }, obj);
 }
